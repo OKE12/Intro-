3fa0f58855b1cae865ec9eea4e643c9c0201b714
OKE12 <37783245+OKE12@users.noreply.github.com>
2018-04-06 00:50:41 -0400

    Update newgame.py

--- newgame.py
+++ newgame.py
@@ -1,10 +1,4 @@
-# A list of replacement words to be passed in to the play game function.
-
-
-# The following are some test strings to pass in to the play_game function.
 easystring = '''
-
-
 A ___1___ is created with the def keyword. You specify the inputs a ___2___ takes by
 adding ___3___ separated by commas between the parentheses. ___4___s by default return ___5___ if you don't specify the value to return. ___6___ can be standard data types such as string, number, dictionary, tuple, and ___7___ or can be more complicated such as objects and lambda functions.'''
 
@@ -21,7 +15,7 @@ def word_in_pos(word, parts_of_speech):
 
 # Plays a full game of mad_libs. A player is prompted to replace words in ml_string,
 # which appear in parts_of_speech with their own words.
-
+count = 0
 
 def play_game(ml_string, parts_of_speech):
     replaced = []
@@ -30,11 +24,20 @@ def play_game(ml_string, parts_of_speech):
         replacement = word_in_pos(word, parts_of_speech)
         if replacement != None:
             user_input = raw_input('''
-
 Eenter anser to number ''' + replacement + " here:")
+            x=1
+            while x<5:
+                if user_input == answers[count]:
+                    word = word.replace(replacement, user_input)
+                    replaced.append(word)
+                    print ml_string [x-1]
 
-            word = word.replace(replacement, user_input)
-            replaced.append(word)
+                    x=x+1
+                else:
+                    replaced.append(word)
+            replaced = " ".join(replaced)
+            return replaced
+
         else:
             replaced.append(word)
     replaced = " ".join(replaced)
@@ -43,12 +46,8 @@ Eenter anser to number ''' + replacement + " here:")
 
 print "Hello and welcome to my game."
 selectlevel = raw_input('''
-
 please select the level of difficulty by typing easy, medium. or hard:''')
 if selectlevel == "easy":
+    answers = answers_1 = ["function", "function", "arguments", "arguments", "None","arguments", "lists"]
     print easystring
     print play_game(easystring, answers_easy)
-
-
-
-#I think they want us to fill in our own items for ml_string, and to put another part of speech.
